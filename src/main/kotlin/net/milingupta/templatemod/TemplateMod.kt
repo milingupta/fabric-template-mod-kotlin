abb5bd1580270e632cd791d48d8fa752751526e4
Milin Gupta <milingupta15@gmail.com>
2023-11-10 11:08:21 -0500

    Changed private variables to public in TemplateMod.kt

--- src/main/kotlin/net/milingupta/templatemod/TemplateMod.kt
+++ src/main/kotlin/net/milingupta/templatemod/TemplateMod.kt
@@ -4,8 +4,8 @@ import net.fabricmc.api.ModInitializer
 import org.slf4j.LoggerFactory
 
 object TemplateMod : ModInitializer {
-	private const val MOD_ID = "templatemod"
-    private val logger = LoggerFactory.getLogger(MOD_ID)
+	const val MOD_ID = "templatemod"
+    val logger = LoggerFactory.getLogger(MOD_ID)
 
 	override fun onInitialize() {
 		logger.info("Hello Fabric world!")
